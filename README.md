a7d55b063afd039bd84368f49fff9d8f4753a293
Tommy Taylor <taylortommy23@gmail.com>
2023-10-24 18:44:22 -0500

    complete ops challenge two

--- README.md
+++ README.md
@@ -1,2 +1,21 @@
 # ops-201d10-code-challenges
 Code Challenges
+#!/bin/bash
+
+# Script Name:                  Hello world
+# Author:                       Tommy Taylor
+# Date of latest revision:      10/24/2023
+# Purpose:                      Print something to screen
+
+# Declaration of variables
+
+# Declaration of functions
+
+
+# Main
+
+echo "Hello World"
+
+
+
+# End
---
+++ helloworld.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+# Script Name:                  hello world
+# Author:                       Tommy Taylor
+# Date of latest revision:      10/24/2023
+# Purpose:                      print something 
+
+# Declaration of variables
+champion="Hello World!"
+winner="I am working perfectly!"
+
+
+# Declaration of functions
+
+
+
+# Main
+echo $champion
+echo $winner
+
+
+
+
+
+# End
