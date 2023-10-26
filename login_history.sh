311be751e8978a08813a998f48e42f4246ae509c
Tommy Taylor <taylortommy23@gmail.com>
2023-10-25 19:05:44 -0500

    Initial commit

---
+++ login_history.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+# Script Name:                  Variables 101
+# Author:                       Tommy Taylor
+# Date of latest revision:      10/25/2023
+# Purpose:                      Learn about variables 
+
+# Declaration of variables
+print_login_history() {
+   echo  "this is the login history:"
+   grep "Accepted password" /var/log/auth.log
+}
+
+
+
+
+# call the function
+print_login_history
+
+
+
+
+# Main
