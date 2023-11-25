e8b9a668cf26b45b07ca3ae463676e8ebb8951c4
taylortommy23 <147282624+taylortommy23@users.noreply.github.com>
2023-11-24 23:25:50 -0600

    Create op_challenge_10.sh

---
+++ op_challenge_10.sh
@@ -0,0 +1,13 @@
+﻿Get-Process | Sort-Object CPU -Descending
+
+Get-Process | Sort-Object id -Descending
+
+Get-Process | Sort-Object WS | Select-Object -Last 5
+
+Start-Process msedge.exe https://owasp.org/www-project-top-ten
+
+for ($i = 1; $i -le 10; $i++) {Start-Process notepad.exe}
+
+Stop-Process -Name notepad
+
+Stop-Process -id 476
