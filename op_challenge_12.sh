83f568bdb27e0f30659d0bd8a87f6b0580b4e672
taylortommy23 <147282624+taylortommy23@users.noreply.github.com>
2023-11-24 23:50:25 -0600

    Create op_challenge_12.sh

---
+++ op_challenge_12.sh
@@ -0,0 +1,11 @@
+﻿$path = "C:\Users\CodyR\Documents\network-report.txt"
+#setting a variable to set the path to the textfile network-report.txt
+
+ipconfig /all > $Path
+#Populate the network-report.txt with everything that ipconfig gives
+
+function get-ipv4 {Select-String -Path $path -Pattern "ipv4"}
+#creating a function using the select screen and using the path variable to set the path to the network-report.txt and using select-string to grabe the line that says ipv4
+get-ipv4
+
+del $path
