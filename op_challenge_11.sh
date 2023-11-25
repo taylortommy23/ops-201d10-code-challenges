94cf703d7957d5613fe8dfcaf3a27e0c299d99f8
taylortommy23 <147282624+taylortommy23@users.noreply.github.com>
2023-11-24 23:35:20 -0600

    Create op_challenge_11.sh

---
+++ op_challenge_11.sh
@@ -0,0 +1,22 @@
+﻿# Turning on file and printer sharing
+Set-NetFirewallRule -DisplayGroup "File And Printer Sharing" -Enabled True -Profile Any
+
+# Allow ICMP
+netsh advfirewall firewall set rule name="File and Printer Sharing (Echo Request - ICMPv4-In)" new enable=yes
+
+# Enable remote management
+Enable-PSRemoting -Force -SkipNetworkProfileCheck
+
+# Remove bloatware
+Get-AppxPackage *calculator* | Remove-AppxPackage
+Get-AppxPackage *people* | Remove-AppxPackage
+Get-AppxPackage *soundrecorder* | Remove-AppxPackage
+Get-AppxPackage *officehub* | Remove-AppxPackage
+Get-AppxPackage *3dbuilder* | Remove-AppxPackage
+Get-AppxPackage *sway* | Remove-AppxPackage
+
+# Enable Hyper-V
+Enable-WindowsOptionalFeature -Online -FeatureName Microsoft-Hyper-V -All
+
+# Disable SMBv1
+Disable-WindowsOptionalFeature -Online -FeatureName SMB1Protocol
